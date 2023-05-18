3d8c36951d76b08eaf2df5fd4dbc24ebd0070bc3
Marco Aurelio Batista <marco.batista.pb@compasso.com.br>
2023-05-18 15:52:22 -0300

    Criação da página Home

--- js/home.js
+++ js/home.js
@@ -5,3 +5,18 @@ página que não seja o signuo ou login, vai perguntar
 se (isLoggedIn === true) se sim, continua tudo certo,
 se não, ele redireciona pro login com
 window.location.href = '' */
+function goToTasks() {
+    window.location.href = "../pages/tasks.html"
+}
+
+function goToCreate() {
+    window.location.href = "../pages/create.html"
+}
+
+function goToNotifications() {
+    window.location.href = "../pages/notifications.html"
+}
+
+function goToProfile() {
+    window.location.href = "../pages/profile.html"
+}
